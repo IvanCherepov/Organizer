1ef083781aa2954f631765e3fc0a16cd04164784
IvanCherepov <cherapau@gmail.com>
2015-11-11 14:20:33 -0800

    update setup

--- mysite/setup.py
+++ mysite/setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='organizer',
     version='0.1.0',
-    description='organizer for your bookmarks',
-    author='IvanCherepov',
+    include_package_data=True,
+    description='Organizer for your bookmarks.',
+    long_description=README,
+    author='Ivan Cherepov',
     author_email='cherapau@gmail.com',
     maintainer='Ivan Cherepov',
     maintainer_email='cherapau@gmail.com',
